2729edc9fdb7dfa24f6ac0dc043e8848dc964a67
Ali Rostami <rostami@axxessio.com>
2022-04-28 18:48:01 +0200

    adding the cooldown time control as well as selection possiblity

--- src/main/resources/web/js/3d_force_graph_action.js
+++ src/main/resources/web/js/3d_force_graph_action.js
@@ -25,13 +25,24 @@ dg =vis.add(visController,'dragCoefficient');
 gr = vis.add(visController,'gravity');
 sc = vis.add(visController,'springCoefficient');
 sl = vis.add(visController,'springLength');
+theta = vis.add(visController,'theta');
+timeStep = vis.add(visController, 'timeStep');
+
+//const settings = new Settings();
+//const controllerOne = gui.add(settings, 'redDistance', 0, 100);
+//controllerOne.onChange(updateLinkDistance);
+
 var buttonSetDefault = { Reset:function(){
   gui.__folders.Visualization.__controllers[0].setValue(defaultVisController.dragCoefficient);
   gui.__folders.Visualization.__controllers[1].setValue(defaultVisController.gravity);
   gui.__folders.Visualization.__controllers[2].setValue(defaultVisController.springCoefficient);
   gui.__folders.Visualization.__controllers[3].setValue(defaultVisController.springLength);
+  gui.__folders.Visualization.__controllers[4].setValue(defaultVisController.springLength);
+  gui.__folders.Visualization.__controllers[5].setValue(defaultVisController.springLength);
 }};
+cooldownTime = {cooldownTime : 15000};
 gui.add(buttonSetDefault, 'Reset')
+gui.add(cooldownTime, 'cooldownTime');
 $('#right_tools').append(gui.domElement);
 
 function threed_force_graph_action(data,ended) {
@@ -40,18 +51,34 @@ function threed_force_graph_action(data,ended) {
           links: data.edges.map(i => ({ source: i.data.source, target:i.data.target} ))
         };
 
-        console.log(visController);
+    let selectedNodes = new Set();
         const Graph = ForceGraph3D()
           (document.getElementById('canvas'))
           .forceEngine('ngraph')
             .ngraphPhysics(visController)
             .graphData(gData)
+            .cooldownTime(cooldownTime.cooldownTime)
 //                        .backgroundColor('#ffffff')
 //                        nodeColor(node => highlightNodes.has(node) ? node === hoverNode ? 'rgb(255,0,0,1)' : 'rgba(255,160,0,0.8)' : 'rgba(0,255,255,0.6)')
             .nodeOpacity(1)
+            .nodeColor(node => selectedNodes.has(node) ? 'yellow' : 'gray')
             .linkColor('rgba(200,200,200,1)')
                     .linkWidth(2)
                     .linkOpacity(1)
+                    .onNodeClick((node, event) => {
+                              if (event.ctrlKey || event.shiftKey || event.altKey) { // multi-selection
+                                selectedNodes.has(node) ? selectedNodes.delete(node) : selectedNodes.add(node);
+                              } else { // single-selection
+                                const untoggle = selectedNodes.has(node) && selectedNodes.size === 1;
+                                selectedNodes.clear();
+                                !untoggle && selectedNodes.add(node);
+                              }
+
+                              Graph.nodeColor(Graph.nodeColor()); // update color of selected nodes
+                            })
+//                     .onLinkClick((node, event) => {
+//
+//                      });
 //                                .linkDirectionalParticles(link => highlightLinks.has(link) ? 4 : 0)
 //                                .linkDirectionalParticleWidth(4)
 //                                .onNodeHover(node => {
@@ -152,13 +179,42 @@ function threed_force_graph_action(data,ended) {
 //                        str += sx + ", " + sy + ", " + sz + "\n";
         });
 
-        str = "\nminimum distance between all nodes:\n" + min_dist;
-        str += "\nmaximum distance between all nodes:\n" + max_dist;
-        str += "\naverage distance between all nodes:\n" + avg;
+        min_dist_selected_nodes = 1000;
+        max_dist_selected_nodes = 0;
+        avg_dist_selected_nodes = 0;
+        sum_dist_selected_nodes = 0;
 
-        str += "\nminimum distance between all edge nodes:\n" + min_dist_e;
-        str += "\nmaximum distance between all edge nodes:\n" + max_dist_e;
-        str += "\naverage distance between all edge nodes:\n" + avg_dist_e;
+        selectedNodes.forEach(function(n1) {
+                  selectedNodes.forEach(function(n2) {
+                    if(n1.id > n2.id) {
+                      dist = Math.sqrt(
+                        Math.pow(n1.__threeObj.position.x-n2.__threeObj.position.x,2) +
+                        Math.pow(n1.__threeObj.position.y-n2.__threeObj.position.y,2) +
+                        Math.pow(n1.__threeObj.position.z-n2.__threeObj.position.z,2)
+                      );
+                      if(min_dist_selected_nodes > dist)
+                        min_dist_selected_nodes = dist;
+                      if(max_dist_selected_nodes < dist)
+                        max_dist_selected_nodes = dist;
+                      sum_dist_selected_nodes += dist;
+                    }
+                  });
+                });
+
+        avg_dist_selected_nodes = sum_dist_selected_nodes/selectedNodes.size;
+
+        str = "Distance between all nodes:\nMinimum: " + min_dist;
+        str += "\nMaximum: " + max_dist;
+        str += "\nAverage: " + avg;
+
+        str += "\n\nDistance between all edge nodes:\nMinimum: " + min_dist_e;
+        str += "\nMaximum: " + max_dist_e;
+        str += "\nAverage: " + avg_dist_e;
+
+        str += "\n\nDistance between selected nodes:\nMinimum: " + min_dist_selected_nodes;
+        str += "\nMaximum: " + max_dist_selected_nodes;
+        str += "\nAverage: " + avg_dist_selected_nodes;
+
 
         $("#vis_inf").html(str)
         });
